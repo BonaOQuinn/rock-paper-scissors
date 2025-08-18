a2f81f773cc4b4fc06b3939ba65908007453bf5e
Bona.OQuinn <bonatoquinn@gmail.com>
2025-08-18 08:07:16 -0700

    prompt method created to get user input
    For the human side of the rock, paper, scissors game, a prompt method was used to recieve and store the human input. by using the getElementById prompt, when the user clicked submit, a function called getHumanChoice was called. And whatever they typed within the textbox was then stored in the userMove variable.

--- RockPaperSciss.js
+++ RockPaperSciss.js
@@ -22,13 +22,19 @@
 
         }
 
+        /*this gets input from the user */
+        /*when the user clicks the submit button, the function 
+          getHumanChoice is called. Within that function, the 
+          userMove var is updated to wtv the user types in the
+          text box.*/
+        let userMove;
 
-        let userMove; /*this gets input from the user */
-        document.getElementById("userSubmit").onclick = function() {
+        document.getElementById("userSubmit").onclick = function getHumanChoice() {
             userMove = document.getElementById("userInput").value;
             console.log(userMove);
         }
 
-        let playerScore = 0; 
-        let computerScore = 0;
+        
+
+        
          
