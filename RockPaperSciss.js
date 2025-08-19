a0d3dbaca33e05b1a669e52e4e66439945cb8246
Bona.OQuinn <bonatoquinn@gmail.com>
2025-08-18 17:41:51 -0700

    commented out old getHumanFunction / created playRound function
    I commented out the lines 33 to 36 because I wasn't sure how to fix the scope of that getHumanChoice function, so instead I created a new getHumanChoice function and used a window.prompt() function and embedded it in the variable userMove. From there I created the playRound function that decides a single game of rock paper scissors between the user and the computer.

--- RockPaperSciss.js
+++ RockPaperSciss.js
@@ -1,12 +1,13 @@
 /*RockPaperSciss.js */
 
 //this function is going to return 1 of three values based on the number generated
+        let randNum; 
         function getComputerChoice() {
             const stringOne = "rock"; 
             const stringTwo = "paper"; 
             const stringThree = "scissors"; 
 
-            let randNum = Math.floor(Math.random() * 3); //Math.floor rounds to nearest whole num
+            randNum = Math.floor(Math.random() * 3); //Math.floor rounds to nearest whole num
 
             if (randNum == 0) {
                 return stringOne; 
@@ -28,13 +29,48 @@
           userMove var is updated to wtv the user types in the
           text box.*/
         let userMove;
+        let newUserMove;
 
-        document.getElementById("userSubmit").onclick = function getHumanChoice() {
+        /*document.getElementById("userSubmit").onclick = function getHumanChoice() {
             userMove = document.getElementById("userInput").value;
-            console.log(userMove);
+            newUserMove = userMove.toLowerCase(); //changes all letters to lower case
+            console.log(newUserMove);
+        } */
+
+        function getHumanChoice() {
+            userMove = window.prompt("Choose your move: Rock, Paper, Or Scissors");
+            newUserMove = userMove.toLowerCase(); 
+            return newUserMove;
         }
 
         
+        let humanScore = 0; 
+        let computerScore = 0;
+
+        function playRound(humanChoice, compChoice) {
+            if (humanChoice == "rock" && compChoice == "scissors" || humanChoice == "scissors" && compChoice == "paper" || humanChoice == "paper" && compChoice == "rock") {
+                humanScore = humanScore + 1; 
+                console.log(`Human wins that round, the score is now Human: ${humanScore} Computer: ${computerScore}`);
+            }
+
+            else if (compChoice == "rock" && humanChoice == "scissors" || compChoice == "scissors" && humanChoice == "paper" || compChoice == "paper" && humanChoice == "rock") {
+                computerScore = computerScore + 1; 
+                console.log(`Computer wins that round, the score is now Computer: ${computerScore} Human: ${humanScore}`);
+            }
+
+            else {
+                console.log("It is a tie!!")
+            }
+
+            console.log(humanChoice + " " + compChoice);
+
+        }
+
+        const humanSelection = getHumanChoice(); 
+        const compSelection = getComputerChoice();
+
+        playRound(humanSelection, compSelection);
+
 
         
          
